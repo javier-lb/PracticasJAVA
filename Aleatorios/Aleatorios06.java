fc9d4932dd7b5ecd3e1cf41158e90a5e196aff72
javier-lb <131590133+javier-lb@users.noreply.github.com>
2023-08-23 17:48:52 +0200

    Add files via upload

---
+++ Aleatorios/Aleatorios06.java
@@ -0,0 +1,20 @@
+public class Aleatorios06{
+	public static void main(String[] args){
+		System.out.println("Genera al azar piedra, papel o tijera:");
+		
+		int mano = (int)(Math.random()*3);
+		
+		switch (mano) {
+			
+		case 0:
+		System.out.println("piedra");
+		break;
+		case 1:
+		System.out.println("papel");
+		break;
+		case 2:
+		System.out.println("tijera");
+		break;
+		}
+	}
+}
