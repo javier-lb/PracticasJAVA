9ad782e701b8d2150769b6e506b89adf758846b9
javier-lb <131590133+javier-lb@users.noreply.github.com>
2023-08-23 17:25:54 +0200

    Add files via upload

---
+++ EjerciciosAleatorios/Matematicas.java
@@ -0,0 +1,8 @@
+public class Matematicas {
+	public static void main (String args[]){
+	 double potencia =Math.pow(2,3);
+	 System.out.println(potencia);
+	 
+	}
+	//Clase Math potencia de 2 al cubo
+}
