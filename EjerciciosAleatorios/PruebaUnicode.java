6bbef65ca393a1b81dfc72f73abfc12ae2ccf97f
javier-lb <131590133+javier-lb@users.noreply.github.com>
2023-08-23 17:27:00 +0200

    Add files via upload

---
+++ EjerciciosAleatorios/PruebaUnicode.java
@@ -0,0 +1,11 @@
+public class PruebaUnicode {
+	
+	public static void main (String args[]){
+		
+		String letra = "\u2192";
+		
+		System.out.println(letra);
+		
+	}
+	//Muestra de codigos Unicode
+}
