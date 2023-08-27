1adab76d893943631beadb3fa303e4836e19ed72
javier-lb <131590133+javier-lb@users.noreply.github.com>
2023-08-27 18:56:01 +0200

    Add files via upload

---
+++ EjerciciosAleatorios/Ejercicio15.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Ejercicio15{
+    public static void main(String[] args) {
+        String dia;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Dime un día de la semana: ");
+        dia = teclado.nextLine();
+
+        switch (dia.toLowerCase()){
+            case "lunes":
+                System.out.println("El "+dia+" es laborable");
+                break;
+            case "martes":
+                System.out.println("El "+dia+" es laborable");
+                break;
+            case "miercoles":
+                System.out.println("El "+dia+" es laborable");
+                break;
+            case "jueves":
+                System.out.println("El "+dia+" es laborable");
+                break;
+            case "viernes":
+                System.out.println("El "+dia+" es laborable");
+                break;
+            case "sabado":
+                System.out.println("El "+dia+" no es laborable");
+                break;
+            case "domingo":
+                System.out.println("El "+dia+" no es laborable");
+                break;
+            default:
+                System.out.println("No compredemos lo que escribiste, lo sentimos");
+                break;
+        }
+        teclado.close();
+    }
+}
---
+++ EjerciciosAleatorios/Ejercicio16.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ejercicio16 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Vamos a hacer números aleatorios entre dos números");
+        System.out.println("Cuantos numeros quieres generar: ");
+        int generador = teclado.nextInt();
+        System.out.println("Dime un primer número: ");
+        double num1= teclado.nextDouble();
+        System.out.println();
+        System.out.println("Dime un segundo número: ");
+        double num2= teclado.nextDouble();
+        System.out.println("\n");
+
+        for (int i = 1; i <= generador; i++) {
+                int aleatorio = (int)(Math.random()*(num2-num1+1)+num1);
+                System.out.println(aleatorio);
+        }
+        teclado.close();
+    }
+}
